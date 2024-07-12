f1fd064e3a456faae086bd124ac4d0e266520ffe
Douglas Jasper <doug.jasper@trios.com>
2024-07-12 19:12:29 -0400

    Day 1 End

---
+++ Greeting/Greeting/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Greeting
+//
+//  Created by Douglas Jasper on 2024-07-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var firstName: UITextField!
+    
+    @IBOutlet weak var lastName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func displayGreeting(_ sender: UIButton) {
+        
+    }
+    
+
+
+}
+
