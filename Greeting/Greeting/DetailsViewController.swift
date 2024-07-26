31cf7551eb162cf53267de99f44a5f21c98f5359
Douglas Jasper <doug.jasper@trios.com>
2024-07-26 19:08:20 -0400

    After Tab Bar Controller and TableView Controller

--- Greeting/Greeting/DetailsViewController.swift
+++ Greeting/Greeting/DetailsViewController.swift
@@ -19,8 +19,31 @@ class DetailsViewController: UIViewController {
     }
     
     @IBAction func confirmDetails(_ sender: UIButton) {
+        showAlert()
     }
     
+    func showAlert()
+    {
+        let email = emailAddress.text!
+        let phone = phoneNumber.text!
+        
+        let title = "Contact Details"
+        let message = "Email: \(email)  Phone: \(phone)"
+        
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(
+            title: "OK",
+            style: .default,
+            handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
     /*
     // MARK: - Navigation
 
